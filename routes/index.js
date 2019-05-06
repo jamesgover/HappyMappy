83d1355b9a783f82822b02c4efa185bd61d8008c
BraeWebb <email@braewebb.com>
2019-05-06 12:41:53 +1000

    Dynamically Load Data from Database

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ let endpoint = "http://" + process.env.GRAPHQL_SERVER + ":5000/graphql";
 console.log("😄🗺️ connecting to GraphQL on " + process.env.GRAPHQL_SERVER);
 const client = new GraphQLClient(endpoint, { headers: {} });
 
+// Query for the location connections between nodes
 const query = `{
   allEthereumconnections {
     nodes {
@@ -24,7 +25,22 @@ const query = `{
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Happy Mappy'});
+  client.request(query).then(data => {
+    // TODO: Fix this data conversion weirdness plz
+    data = data["allEthereumconnections"]["nodes"];
+    let result = [];
+    for (let d in data) {
+      let item = data[d];
+      let start = item['ethereumnodeByNeighbour'];
+      let end = item['ethereumnodeByNode'];
+      result.push([[start['lat'], start['long']], [end['lat'], end['long']]])
+    }
+    console.log(result);
+    res.render('map', {
+      title: 'Happy Mappy',
+      locationConnections: result
+    });
+  }).catch(next);
 });
 
 router.get('/query', function(req, res, next) {
