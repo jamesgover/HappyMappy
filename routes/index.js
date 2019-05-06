8cd1db48efa8467f8f1cef06cd62fd7c174002b8
BraeWebb <email@braewebb.com>
2019-05-06 12:19:46 +1000

    Dockerized Node Application
    Dockerized the node application and connected it to the docker compose network.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 let express = require('express');
 let router = express.Router();
 
+let {request, GraphQLClient} = require('graphql-request');
+
+let endpoint = "http://" + process.env.GRAPHQL_SERVER + ":5000/graphql";
+console.log("😄🗺️ connecting to GraphQL on " + process.env.GRAPHQL_SERVER);
+const client = new GraphQLClient(endpoint, { headers: {} });
+
+const query = `{
+  allEthereumconnections {
+    nodes {
+      ethereumnodeByNeighbour {
+        lat
+        long
+      }
+      ethereumnodeByNode {
+        lat
+        long
+      }
+    }
+  }
+}`;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {title: 'Happy Mappy'});
+});
+
+router.get('/query', function(req, res, next) {
+  client.request(query).then(data => res.send(data));
 });
 
 module.exports = router;
